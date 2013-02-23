f230abc01e1e428fcfc4b8d116f42459db3521a7
Dan Korostelev <nadako@gmail.com>
2013-02-23 16:59:10 +0400

    Fixes.

--- res/levels.js
+++ res/levels.js
@@ -7,9 +7,16 @@ var levelDefs = [
                  animDuration: 3000,
                  frames: [
                      {image: "test1.png", deadly: false},
+                     {image: "test2.png", deadly: false},
+                     {image: "test3.png", deadly: true}
+                 ]
+             },
+             {
+                 animDuration: 6000,
+                 frames: [
                      {image: "test1.png", deadly: false},
-                     {image: "test1.png", deadly: true},
-                     {image: "test1.png", deadly: true}
+                     {image: "test2.png", deadly: false},
+                     {image: "test3.png", deadly: true}
                  ]
              }
          ]
@@ -19,12 +26,11 @@ var levelDefs = [
         duration: 15000,
         obstacles: [
             {
-                animDuration: 3000,
+                animDuration: 1000,
                 frames: [
                     {image: "test1.png", deadly: false},
-                    {image: "test1.png", deadly: false},
-                    {image: "test1.png", deadly: true},
-                    {image: "test1.png", deadly: true}
+                    {image: "test2.png", deadly: false},
+                    {image: "test3.png", deadly: true}
                 ]
             }
         ]
